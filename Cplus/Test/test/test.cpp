2bfe6e7ee0a44d0d3bd7ea1666996b803c599a5f
nypawar <nypawar@gmail.com>
2020-05-06 02:48:56 +0530

    test prg

---
+++ Cplus/Test/test/test.cpp
@@ -0,0 +1,24 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 06-May-2020
+ *      Author: nitin
+ */
+
+#include<iostream>
+using namespace std;
+class Test{
+public:
+	Test(){
+		cout<<"Welcome in Main Class"<<endl;
+	}
+};
+int main(void)
+{
+	Test t;
+
+	return 1;
+
+}
+
+
