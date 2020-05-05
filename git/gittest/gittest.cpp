7037e9af6e271b02718deeb8c8f3de9c6e7760ce
nypawar <nypawar@gmail.com>
2020-05-06 04:27:11 +0530

    Added test

---
+++ git/gittest/gittest.cpp
@@ -0,0 +1,17 @@
+/*
+ * gittest.cpp
+ *
+ *  Created on: 06-May-2020
+ *      Author: nitin
+ */
+
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	cout<<"WELCOME"<<endl;
+	return 1;
+}
+
+
